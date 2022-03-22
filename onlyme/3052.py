276e67c0261d56e15058e6b77943ff47c89236b8
Haneol Choi <ulywooly@gmail.com>
2022-03-23 03:23:11 +0900

    create 3052.py

--- onlyme/3052.py
+++ onlyme/3052.py
@@ -1,15 +1,16 @@
 list = []; reminder_list = []
 count = 0
 for i in range(10):
-    s = input()
+    s = int(input())
     reminder_list.append(s%42)
 for i in range(10):
     for j in range(i+1, 10):
-        if(reminder_list[i] == -1):
+        if reminder_list[i] == -1:
             continue
-        elif(reminder_list[i] == reminder_list[j]):
+        elif reminder_list[i] == reminder_list[j]:
             list[j] = -1
-for i in range(10):
-    if(list[i] != -1):
+for i in list:
+    if i != -1:
         count += 1
 print(count)
+
