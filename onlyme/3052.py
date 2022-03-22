1682e4b4d7b9dbed771eba0511499510fbfc94c4
Haneol Choi <ulywooly@gmail.com>
2022-03-23 03:12:42 +0900

    create 3052.py

---
+++ onlyme/3052.py
@@ -0,0 +1,15 @@
+list = []; reminder_list = []
+count = 0
+for i in range(10):
+    s = input()
+    reminder_list.append(s%42)
+for i in range(10):
+    for j in range(i+1, 10):
+        if(reminder_list[i] == -1):
+            continue
+        elif(reminder_list[i] == reminder_list[j]):
+            list[j] = -1
+for i in range(10):
+    if(list[i] != -1):
+        count += 1
+print(count)
