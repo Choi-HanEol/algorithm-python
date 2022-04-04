2f5b61dfc1adc4862ba736b70d6792599c727a48
Haneol Choi <ulywooly@gmail.com>
2022-04-05 01:02:29 +0900

    complete the week1

---
+++ withgroup/week2/1931.py
@@ -0,0 +1,22 @@
+import sys
+
+time = []
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    input_time = tuple(map(int, sys.stdin.readline().split()))
+    time.append(input_time)
+
+time.sort(key=lambda x: x[0])
+time.sort(key=lambda x: x[1])
+
+cnt = 1
+
+cnt = 0
+end = 0
+for i, j in time:
+
+    if i >= end:
+        cnt += 1
+        end = j
+print(cnt)
